60d14f929011d03d7536b8ebd3d4d1a95f736f57
Andrei Calinescu <andreicali@gmail.com>
2018-09-13 14:39:07 -0400

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default () => 
+
+<div><h1>Hello FCC and GitHub World!</h1>
+    <div><p>My name is Andrei Calinescu. I started Free Code Camp in January 2017. I'm really looking forward to the hackathon!</p></div>
+    <div><p>For some reason, the preview in my browser didn't update when I changed the contents of my index.js file, so I had to use normal Gatsby instead of Ubuntu Gatsby as in the instructions. Hope this is OK!</p></div>
+</div>
