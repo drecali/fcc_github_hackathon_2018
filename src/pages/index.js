3d59c1250696d70075c4efdb3701a323cfe67df4
Andrei Calinescu <andreicali@gmail.com>
2018-09-14 10:24:37 -0400

    mentioned README.md update on site

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,4 +6,5 @@ export default () =>
     <div><p>My name is Andrei Calinescu. I started Free Code Camp in January 2017. I'm really looking forward to the hackathon!</p></div>
     <div><p>For some reason, the preview in my browser didn't update when I changed the contents of my index.js file, so I had to use normal Gatsby instead of Ubuntu Gatsby as in the instructions. Hope this is OK!</p></div>
     <p>I just applied for my free ticket to the hackathon. Hope I get it! :D</p>
+    <p>PS. I just added more info and links to README.md</p>
 </div>
