25535108bf1349dfcb8c3209215141535cd38899
Andrei Calinescu <andreicali@gmail.com>
2018-09-14 09:28:26 -0400

    just applied

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,4 +5,5 @@ export default () =>
 <div><h1>Hello FCC and GitHub World!</h1>
     <div><p>My name is Andrei Calinescu. I started Free Code Camp in January 2017. I'm really looking forward to the hackathon!</p></div>
     <div><p>For some reason, the preview in my browser didn't update when I changed the contents of my index.js file, so I had to use normal Gatsby instead of Ubuntu Gatsby as in the instructions. Hope this is OK!</p></div>
+    <p>I just applied for my free ticket to the hackathon. Hope I get it! :D</p>
 </div>
